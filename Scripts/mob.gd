67f3c256a3dda2c145de6b2b30ac98ce2f6e53a2
Dany <danixps1@gmail.com>
2025-11-25 03:21:08 -0600

    Animación de player y mob

--- Scripts/mob.gd
+++ Scripts/mob.gd
@@ -1,7 +1,15 @@
 extends RigidBody3D
 
 @onready var bee_model: Node3D = $BeeModel
+@onready var player = get_node("/root/Main/Player")
+
+var speed = randf_range(2.0, 4.0)
+
+func _physics_process(delta):
+	var direction = global_position.direction_to(player.global_position)
+	direction.y = 0.0
+	linear_velocity = direction * speed
+	
 
 func attack():
 	bee_model.bite()
-	
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,18 +1,46 @@
 extends CharacterBody3D
 
+@onready var cam = $CameraPivot
+@onready var animation_tree: AnimationTree = $Character/AnimationTree
+
+
 const SPEED := 5.5
 const JUMP_VELOCITY := 10.0
 var mouse_sens := 0.1
 var mouse_visible := false
 
-@onready var cam = $CameraPivot
+enum {IDLE, RUN, JUMP}
+var curAnim = IDLE
+
+@export var blend_speed: int = 15
+
+var run_val := 0.0
+var jump_val := 0.0
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 func _physics_process(delta:float):
 	movimiento(delta)
+	handle_animations(delta)
+	update_tree()
 	move_and_slide()
+	
+func handle_animations(delta):
+	match curAnim:
+		IDLE:
+			run_val = lerpf(run_val, 0, blend_speed*delta)
+			jump_val = lerpf(jump_val, 0, blend_speed*delta)
+		RUN:
+			run_val = lerpf(run_val, 1, blend_speed*delta)
+			jump_val = lerpf(jump_val, 0, blend_speed*delta)
+		JUMP:
+			run_val = lerpf(run_val, 0, blend_speed*delta)
+			jump_val = lerpf(jump_val, 1, blend_speed*delta)
+
+func update_tree():
+	animation_tree["parameters/Run/blend_amount"] = run_val
+	animation_tree["parameters/Jump/blend_amount"] = jump_val
 
 #Camera movement
 func _unhandled_input(event: InputEvent) -> void:
@@ -37,8 +65,10 @@ func movimiento(delta:float) -> void:
 	velocity.y -= 10.0 * delta
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	elif Input.is_action_just_released("jump") and velocity.y > 0.0:
-		velocity.y = 0.0
+		curAnim = JUMP
+		return
+	elif Input.is_action_just_released("jump") and velocity.y > 3.0:
+		velocity.y *= 0.5
 	
 	#Direction 
 	var input_direction_2D = Input.get_vector(
@@ -50,6 +80,10 @@ func movimiento(delta:float) -> void:
 	if input_direction_3D:
 		velocity.x = input_direction_3D.x * SPEED
 		velocity.z = input_direction_3D.z * SPEED
+		if is_on_floor():
+			curAnim = RUN
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			curAnim = IDLE
